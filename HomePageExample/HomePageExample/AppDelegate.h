35384b4398bae6fcec83aa683b9b7b33b8979878
PatrickDale <patdale216@gmail.com>
2014-04-30 18:33:42 -0500

    Example for a possible homepage layout. Has a scrollable view and tabs to navigate to different pages.

---
+++ HomePageExample/HomePageExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomePageExample
+//
+//  Created by Pat Dale on 4/30/14.
+//  Copyright (c) 2014 4students2apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomePageExample/HomePageExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HomePageExample
+//
+//  Created by Pat Dale on 4/30/14.
+//  Copyright (c) 2014 4students2apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
