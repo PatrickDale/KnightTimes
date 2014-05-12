7e26072095a8b2c7623e9875e39ee817ecb7120d
Garo Anguiano-Sainz <edgardo@eduroam-171-49.dyn.carleton.edu>
2014-05-11 20:58:47 -0500

    Schedule

---
+++ HomePageExample/HomePageExample/ScheduleViewController.m
@@ -0,0 +1,28 @@
+//
+//  ScheduleViewController.m
+//  HomePageExample
+//
+//  Created by Garo Anguiano-Sainz on 5/11/14.
+//  Copyright (c) 2014 4students2apps. All rights reserved.
+//
+
+#import "ScheduleViewController.h"
+
+@interface ScheduleViewController ()
+
+@end
+
+@implementation ScheduleViewController
+
+@synthesize labelOne,labelTwo,labelThree,labelFour,labelFive,labelSix,labelSeven;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+@end
