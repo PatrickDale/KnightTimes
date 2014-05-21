0e86f1ea7694364b3ef18864316e87f4fff34baa
PatrickDale <patdale216@gmail.com>
2014-05-20 20:48:25 -0500

    Built homeview to layout programmatically

--- HomePageExample/HomePageExample/ViewController.h
+++ HomePageExample/HomePageExample/ViewController.h
@@ -10,22 +10,8 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIView *storyOneView;
-@property (weak, nonatomic) IBOutlet UIView *storyTwoView;
-@property (weak, nonatomic) IBOutlet UIView *storyThreeView;
-@property (weak, nonatomic) IBOutlet UIView *storyFourView;
-@property (weak, nonatomic) IBOutlet UIView *storyFiveView;
-@property (weak, nonatomic) IBOutlet UIView *storySixView;
-@property (weak, nonatomic) IBOutlet UIView *storySevenView;
-@property (weak, nonatomic) IBOutlet UIView *storyEightView;
-@property (weak, nonatomic) IBOutlet UIView *storyNineView;
-@property (weak, nonatomic) IBOutlet UIView *storyTenView;
-@property (weak, nonatomic) IBOutlet UIView *storyElevenView;
-@property (weak, nonatomic) IBOutlet UIView *storyTwelveView;
-@property (weak, nonatomic) IBOutlet UIView *storyThirteenView;
-@property (weak, nonatomic) IBOutlet UIView *storyFourteenView;
 @property (weak, nonatomic) IBOutlet UIImageView *storyImage;
-
+@property (strong, nonatomic) IBOutlet UIView *homeViewController;
 - (IBAction)returnToHome:(id)sender;
 - (IBAction)gestureRecognized:(id)sender;
 
--- HomePageExample/HomePageExample/ViewController.m
+++ HomePageExample/HomePageExample/ViewController.m
@@ -9,13 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController () {
-NSArray *storyViewArray;
+NSMutableArray *storyViewArray;
 }
 @end
 
 @implementation ViewController
 
-@synthesize storyOneView, storyTwoView, storyThreeView, storyFourView, storyFiveView, storySixView, storySevenView, storyEightView, storyNineView, storyTenView, storyElevenView, storyTwelveView, storyThirteenView, storyFourteenView;
+@synthesize homeViewController;
 @synthesize storyImage;
 
 /***
@@ -26,31 +26,77 @@ NSArray *storyViewArray;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	storyViewArray = [NSArray arrayWithObjects:storyOneView, storyTwoView, storyThreeView, storyFourView, storyFiveView, storySixView, storySevenView, storyEightView, storyNineView, storyTenView, storyElevenView, storyTwelveView, storyThirteenView, storyFourteenView, nil];
-    for (int i = 0; i < [storyViewArray count]; i++) {
-        UIView* field = [storyViewArray objectAtIndex: i];
-        field.tag = i;
-        field.backgroundColor = [UIColor colorWithRed:21.0/255.0 green:67.0/255.0 blue:115.0/255.0 alpha:1];
+    //Set background collor of homeview
+    homeViewController.backgroundColor = [UIColor colorWithRed:21.0/255.0 green:67.0/255.0 blue:115.0/255.0 alpha:1];
+    //Create scrollable subview
+    UIScrollView *scrollSubView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].applicationFrame.size.width, [UIScreen mainScreen].applicationFrame.size.height)];
+    int count = 1;
+    int height = 20;
+    //Create six smaller subviews
+    for (int i = 0; i<6; i++) {
+        // Demensions of left and right boxes
+        CGRect  leftBox = CGRectMake(10, height, [self box_width]-5, [self box_height]);
+        CGRect  rightBox = CGRectMake(([UIScreen mainScreen].applicationFrame.size.width/2)+5, height, [self box_width]-5, [self box_height]);
+        height += [self box_height]+10;
+        //Build left and right subview
+        UIView* leftSubView = [[UIView alloc] initWithFrame:leftBox];
+        UIView* rightSubView = [[UIView alloc] initWithFrame:rightBox];
+        //Create unique tag for each
+        leftSubView.tag = count;
+        count++;
+        rightSubView.tag = count;
+        count++;
+        //leftSubView.backgroundColor = [UIColor colorWithRed:245.0/255.0 green:188.0/255.0 blue:53.0/255.0 alpha:1];
+        leftSubView.backgroundColor = [UIColor whiteColor];
+        //Create a gesture for each view
+        UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognized:)];
+        [leftSubView addGestureRecognizer:singleTap];
+        [leftSubView setMultipleTouchEnabled:YES];
+        [leftSubView setUserInteractionEnabled:YES];
+        leftSubView.layer.borderColor = [UIColor colorWithRed:245.0/255.0 green:188.0/255.0 blue:53.0/255.0 alpha:1].CGColor;
+        leftSubView.layer.borderWidth = 3.0f;
+        rightSubView.backgroundColor = [UIColor whiteColor];
+        UITapGestureRecognizer *singleTap2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(gestureRecognized:)];
+        [rightSubView addGestureRecognizer:singleTap2];
+        [rightSubView setMultipleTouchEnabled:YES];
+        [rightSubView setUserInteractionEnabled:YES];
+        rightSubView.layer.borderColor = [UIColor colorWithRed:245.0/255.0 green:188.0/255.0 blue:53.0/255.0 alpha:1].CGColor;
+        rightSubView.layer.borderWidth = 3.0f;
+        //Add them to the scrollable subview and array
+        [scrollSubView addSubview:leftSubView];
+        [scrollSubView addSubview:rightSubView];
+        [storyViewArray addObject:leftSubView];
+        [storyViewArray addObject:rightSubView];
     }
+    //Add scrollable subview with all subviews to homeview
+    scrollSubView.contentSize = CGSizeMake([UIScreen mainScreen].applicationFrame.size.width, height+30);
+    [homeViewController addSubview:scrollSubView];
     storyImage.image = [UIImage imageNamed:@"CJ_Dale_Shot_Put.jpg"];
 }
 
-/*
- UNIMPLEMENTED CODE
+
 - (void)viewDidLayoutSubviews
 {
     [super viewDidLayoutSubviews];
     
-    UILabel *yourLabel = [[UILabel alloc] initWithFrame:CGRectMake(storyTwoView.frame.origin.x, storyTwoView.frame.origin.y, storyTwoView.frame.size.width, storyTwoView.frame.size.height)];
-    [yourLabel setText:@"ONE TWO THREE FOUR FIVE"];
-    [yourLabel setTextColor:[UIColor yellowColor]];
+    //TEXT IN SUBVIEWS
+    UILabel *yourLabel = [[UILabel alloc] initWithFrame:CGRectMake((((UIView*)[self.view viewWithTag:1]).frame.origin.x)-2, (((UIView*)[self.view viewWithTag:1]).frame.origin.y+((((UIView*)[self.view viewWithTag:1]).frame.size.height)*(3.0/6.0))), (((UIView*)[self.view viewWithTag:1]).frame.size.width)-10, (((UIView*)[self.view viewWithTag:1]).frame.size.height)*(1.0/3.0))];
+    [yourLabel setText:@"Dsida, Tsutsui, Baker, and Stillerman named to D3baseball.com All-Region Team"];
+    [yourLabel setTextColor:[UIColor colorWithRed:21.0/255.0 green:67.0/255.0 blue:115.0/255.0 alpha:1]];
     [yourLabel setBackgroundColor:[UIColor clearColor]];
     [yourLabel setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
+    //yourLabel.shadowColor = [UIColor blackColor];
+    //yourLabel.shadowOffset = CGSizeMake(0, 1.0);
     yourLabel.numberOfLines = 0;
     //yourLabel.lineBreakMode = NSLineBreakByCharWrapping;
-    [storyTwoView addSubview:yourLabel];
+    [((UIView*)[self.view viewWithTag:1]) addSubview:yourLabel];
+    
+    //IMAGE IN SUBVIEWS
+    UIImageView *imageArea = [[UIImageView alloc] initWithFrame:CGRectMake((((UIView*)[self.view viewWithTag:1]).frame.origin.x), (((UIView*)[self.view viewWithTag:1]).frame.origin.y)-10, (((UIView*)[self.view viewWithTag:1]).frame.size.width)-20, (((UIView*)[self.view viewWithTag:1]).frame.size.height)*(3.0/6.0))];
+    imageArea.image = [UIImage imageNamed:@"CJ_Dale_Shot_Put.jpg"];
+    [((UIView*)[self.view viewWithTag:1]) addSubview:imageArea];
 }
-*/
+
 
 /***
  * Return to home view when in story view.
@@ -78,6 +124,14 @@ NSArray *storyViewArray;
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (CGFloat) box_height   {
+    return [UIScreen mainScreen].applicationFrame.size.height/3;
+}
+
+- (CGFloat) box_width   {
+    return ([UIScreen mainScreen].applicationFrame.size.width/2)-10;
 }
 
 @end
